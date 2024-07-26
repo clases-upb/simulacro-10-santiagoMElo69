7c99738e7b49cbc44e73c65048f4291f00270fdf
santiagoMElo69 <santiago.lopeza.col@upb.edu.co>
2024-07-26 11:26:37 -0500

    segundo envio

--- app/src/main/java/simulacro/App.java
+++ app/src/main/java/simulacro/App.java
@@ -6,13 +6,11 @@ package simulacro;
 public class App {
     public static void main(String[] args) {
     }
-
     /*
      * 1. Diseña el algoritmo y programa la función Convertir_min_sec que recibe un número entero en minutos 
      * y devuelve en entero, los segundos equivalentes. Si hay un error, devuelve -1. 
      *  Adaptado de edabit
      */
-    
     public static int Convertir_min_sec (int minutos){
 
         try {
@@ -25,13 +23,11 @@ public class App {
                 return -1;
             }
     }
-
      /*
      * 2. Diseña el algoritmo y programa la función Validar_iguales que recibe dos números shorts 
      * y devuelve un booleano true si son iguales, false si no lo son. Si hay un error, devuelve false también.
      *  Adaptado de edabit
      */
-
     public static boolean Validar_iguales (short nr1, short nr2){
         try {
             boolean son_iguales = false ;
@@ -43,14 +39,12 @@ public class App {
             return false;
         }
     }
-
     /*
      * 3. Diseña el algoritmo y programa la función Calcular_potencia que recibe en un short el voltaje y en 
      * un byte, la corriente. Devuelve en un float, la potencia (potencia = voltaje * corriente). 
      * Si hay un error, devuelve 0.
      *  Adaptado de edabit
      */
-
     public static float Calcular_potencia (short voltaje, byte corriente){
        try {
             float potencia = 0;
